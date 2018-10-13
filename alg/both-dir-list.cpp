302721ebd7d1029d1b805592dbde1eb71f29ad60
shunak <shuna966@gmail.com>
2018-10-14 00:01:22 +0900

    1810133

--- alg/both-dir-list.cpp
+++ alg/both-dir-list.cpp
@@ -4,7 +4,36 @@
 struct Node {
     int key;
     Node *prev, *next;
+};
+
+// initialize both-dir list
+Node *nil;
+
+void init(){
+    nil = (Node *)malloc(sizeof(Node));
+    nil->next=nil;
+    nil->prev=nil;
 }
+
+// insert 
+void insert(int key){
+    Node *x = (Node *)malloc(sizeof(Node));
+    x->key=key;
+    // add element just after sentinnel
+    x->next=nil->next;
+    nil->next->prev=x;
+    nil->next=x;
+    x->prev=nil;
+}
+
+
+
+
+
+
+
+
+
 
 
 
