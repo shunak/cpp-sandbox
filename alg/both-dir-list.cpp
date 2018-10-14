a2d0b0cbd02aad8732d9b67778931f398ec30bf6
shunak <shuna966@gmail.com>
2018-10-14 17:51:18 +0900

    181014

--- alg/both-dir-list.cpp
+++ alg/both-dir-list.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 // def of both-direction Node 
 struct Node {
@@ -25,6 +27,113 @@ void insert(int key){
     nil->next=x;
     x->prev=nil;
 }
+
+
+// search list
+Node* listSearch(int key){
+    Node *cur = nil->next;
+    while(cur != nil && cur->key != key){
+        cur = cur->next;
+    }
+    return cur;
+}
+
+
+// delete list
+void deleteNode(Node *t){
+    if(t==nil) return;
+    t->prev->next=t->next;
+    t->next->prev=t->prev;
+    free(t);
+}
+
+void deleteFirst(){
+    deleteNode(nil->next);
+}
+
+void deleteLast(){
+    deleteNode(nil->prev);
+}
+
+void deleteKey(int key){
+    deleteNode(listSearch(key));
+}
+
+// print result
+void printList(){
+    Node * cur = nil->next;
+    int isf = 0;
+    while(1){
+        if(cur==nil)break;
+        printf("%d",cur->key);
+        if(++isf>0)printf(" ");
+        cur=cur->next;
+    }
+    printf("\n");
+}
+
+
+int main(){
+    int key, n, i;
+    // int size = 0;
+    char con[20];
+    // int np = 0, nd = 0;
+    scanf("%d",&n);
+    init();
+    for(i=0;i<n;i++){
+        scanf("%s%d", con, &key);
+        if(con[0]=='i') {
+
+            insert(key);
+            // np++;
+            // size++;
+
+        }else if(con[0]=='d'){
+
+            if(strlen(con)>6){
+                if(con[6]=='F'){
+                    deleteFirst();
+                }else if(con[6]=='L'){
+                    deleteLast();
+                }
+            }else{
+                deleteKey(key);
+                // nd++;
+            }
+
+            // size--;
+
+        }
+
+    }
+
+
+    printList();
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
