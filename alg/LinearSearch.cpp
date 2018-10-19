d396ee2a8128a6ae2c52e57b52ec160baf18abfc
shunak <shuna966@gmail.com>
2018-10-19 23:58:33 +0900

    181019

---
+++ alg/LinearSearch.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int n;
+int S[10^9];
+int q;
+int T[10^9];
+
+cin >> n;
+
+// cout << n << endl;
+while(n>0){
+
+    cin >> S[n];
+    n--;
+
+}
+
+while(n>0){
+
+cout << S[n] << endl;
+
+n--;
+
+}
+
+
+
+
+
+
+
+// while(q>0){
+//     cin >> T[q];
+//     q--;
+// }
+
+
+// cout << T[0] << endl;
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
