c399502cc54ae18433389475aeada02c865de2a3
Nakamura syuhei <syu@NakamuranoMacBook-puro.local>
2018-10-27 18:20:09 +0900

    181027

---
+++ practical/strlen.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+int main(){
+    
+    char s[256];
+    int len;
+
+    printf("文字列を入力:");
+    scanf("%s",s);
+    len = strlen(s);
+    printf("文字列の長さ:");
+    printf("%d文字\n",len);
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
