daf1c980c7d43a636aea9d2b2d6f04e40485faab
shunak <shuna966@gmail.com>
2018-11-01 23:03:22 +0900

    181101

---
+++ alg/otoshidama.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+int N, Y, x, y, z;
+
+
+int main(){
+    
+    scanf("%d %d",&N, &Y);
+
+    x = N * 10000;
+    y = N * 5000;
+    z = N * 1000;
+
+    printf("%d %d %d\n", x, y, z);
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
