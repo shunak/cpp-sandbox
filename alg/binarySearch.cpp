dea2f0189bea2a66564c25fcb246a701a6ddf5bb
shunak <shuna966@gmail.com>
2018-11-03 00:12:11 +0900

    181102

--- alg/binarySearch.cpp
+++ alg/binarySearch.cpp
@@ -4,14 +4,32 @@
 
 int n, S[1000000], q, T[50000];
 
-
-
-
-
 int main(){
 
+    scanf("%d",&n);
+
+    while(n>0){
+    
+    // for(int i = 0; i < n; i++)
+    // {
+        scanf("%d",&S[n]);
+    // }
+    
+        n--;
+    }
+
+    scanf("%d",&q);
+    
+    while(q>0){
+
+        scanf("%d",&T[q]);
+
+        q--;
+
+    }
 
 
+    return 0;
 
 }
 
