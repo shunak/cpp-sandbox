3c1d3c5b8b5619654ef42e6f8fe7ba10257d5781
shunak <shuna966@gmail.com>
2018-11-06 23:56:20 +0900

    181106

--- alg/otoshidama.cpp
+++ alg/otoshidama.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <cstring>
 
-int N, Y, x, y, z;
+int N, Y, x, y, z, rest;
 
 
 int main(){
@@ -16,6 +16,31 @@ int main(){
     printf("%d %d %d\n", x, y, z);
 
     printf("%d\n",Y%10000);
+    printf("%d\n",Y/10000);
+
+    rest = N-Y/10000;
+
+    if (rest > 500) {
+
+        // rest = N/2-Y/1000 + N/2-Y/5000;
+        rest = N-Y/1000;
+        
+        if (rest<0) {
+
+            rest = N-Y/5000;
+        
+        }
+        
+
+    }
+
+
+
+    printf("%d\n",rest);
+
+
+
+
 
 
     return 0;
