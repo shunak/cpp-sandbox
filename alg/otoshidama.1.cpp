db8bd6f6f17291a922b11a0faa08d9debb060099
shunak <shuna966@gmail.com>
2018-11-08 23:23:12 +0900

    181108

---
+++ alg/otoshidama.1.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+int N, Y, x, y, z, rest;
+
+int main(){
+    
+    scanf("%d %d",&N, &Y);
+
+    N = x + y + z;
+    Y = 10000*x+5000*y+1000*z;
+
+    
+
+
+
+
+    // x = N * 10000;
+    // // x = 10 * N * 1000;
+    // y = N * 5000;
+    // // y = 5 * N * 1000;
+    // z = N * 1000;
+    // // z = 1 * N * 1000;
+
+    printf("%d %d %d\n", x, y, z);
+
+    // printf("%d\n",Y%10000);
+    // printf("%d\n",Y/10000);
+
+    // rest = N-Y/10000;
+
+    // if (rest > 500) {
+
+    //     rest = N-Y/1000;
+        
+    //     if (rest<0) {
+
+    //         rest = N-Y/5000;
+        
+    //     }
+        
+
+    // }
+    
+    // どう均等にわけるか？
+
+
+    printf("%d\n",rest);
+
+
+
+
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- alg/otoshidama.cpp
+++ alg/otoshidama.cpp
@@ -2,50 +2,46 @@
 #include <cstdlib>
 #include <cstring>
 
-int N, Y, x, y, z, rest;
+int N, Y, x, y, z, sum, count, otoshidama;
 
 int main(){
     
     scanf("%d %d",&N, &Y);
 
-    N = x + y + z;
-    Y = 10000*x+5000*y+1000*z;
+    
+// 全ての組み合わせを求める
+for(int x = 0; x < N; x++)
+{
+    
+    for(int y = 0; y < N; y++)
+    {
 
-    // x = N * 10000;
-    // // x = 10 * N * 1000;
-    // y = N * 5000;
-    // // y = 5 * N * 1000;
-    // z = N * 1000;
-    // // z = 1 * N * 1000;
+        for(int z = 0; z < N; z++)
+        {
 
-    printf("%d %d %d\n", x, y, z);
+            sum = x+y+z;
+            otoshidama = 10000*x+5000*y+1000*z;
+            
+            if (N==sum && Y==otoshidama) {
 
-    printf("%d\n",Y%10000);
-    printf("%d\n",Y/10000);
+                printf("%d %d %d\n",x,y,z);
 
-    rest = N-Y/10000;
+            }
+            
 
-    if (rest > 500) {
-
-        rest = N-Y/1000;
-        
-        if (rest<0) {
-
-            rest = N-Y/5000;
-        
         }
-        
 
     }
     
-    // どう均等にわけるか？
-
-
-    printf("%d\n",rest);
+}
 
 
 
 
+
+
+
+    // printf("%d %d %d\n", x, y, z);
 
 
     return 0;
