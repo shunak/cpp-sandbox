b79625e9c33f8c4d699e9c02cdbe893c0fe6fdf9
shunak <shuna966@gmail.com>
2018-11-11 23:54:54 +0900

    181111

---
+++ alg/list.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+int main()
+{   
+
+    
+    struct list{
+        char value[100];
+        int next=-1;
+    };
+    
+    struct list List;
+    char mvalue;
+    int ls = 0;
+    int First = -1;
+
+    List[ls].value = mvalue;
+
+
+
+
+
+
+
+
+    return 0;
+}
