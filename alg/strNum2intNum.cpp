b398db1d5c5bececc50b134d4d681638536ee495
shunak <shuna966@gmail.com>
2018-11-14 00:45:03 +0900

    181113

--- alg/strNum2intNum.cpp
+++ alg/strNum2intNum.cpp
@@ -1,32 +1,48 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <vector>
 #define N 10
+using namespace std;
 
+// int henkan(int M){
 int henkan(char M){
 
     int i;
-    char digit[N];
+    vector<char> digit;
+    // char digit[N];
+    digit={'0','1','2','3','4','5','6','7','8','9','0'};
+    // for(int i = 1; i <= N; i++)
+    // {
+    //     digit.push_back((char)i);
+    //     // digit[i]='i';
 
-    for(int i = 1; i <= N; i++)
-    {
-        digit[i]='i';
-    }
+    // }
+    // printf("%s",&digit[2]);
 
     i=1;
     while(M!=digit[i]) {
-        i++;
+        i+=1;
     }
 
-    return i%10;
+    if(i==10){
+        return 0+2;
+    }else{
+        return i+2;
+    }
+    // return i%10;
 }
+
+
 
 
 int main(){
 
     char M;
+    // int M;
     int IM;
 
+    // scanf("%d",&M);
     scanf("%s",&M);
 
     IM = henkan(M);
