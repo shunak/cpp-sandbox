e377a8cf9f8960b2781334d36f1c91e3196fe367
shunak <shuna966@gmail.com>
2018-11-18 14:01:56 +0900

    811118

--- alg/pointer_181118.cpp
+++ alg/pointer_181118.cpp
@@ -15,12 +15,14 @@ int main(void)
     char *answer = &(stack[2]);
 
     /* アドレス */
-    *data0 = (char)0x1;                /* 1000     */
-    *data1 = (char)0x2;                /* 1001     */
+    *data0 = (char)0x9;                /* 1000     */
+    *data1 = (char)0x1;                /* 1001     */
     *answer = (char)(*data0 + *data1); /* 1002     */
 
     // ポインタ型の値を10進数で表示
     printf("%d\n",*answer);
+    // ポインタ型の値を16進数で表示
+    printf("%x\n",*answer);
 
     return (0); /* 1003     */
 }
