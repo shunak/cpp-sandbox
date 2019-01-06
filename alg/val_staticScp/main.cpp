7ad7902c56cda84ae9001a17fcefcfbb6e878e87
shunak <shuna966@gmail.com>
2019-01-06 23:37:56 +0900

    190106

---
+++ alg/val_staticScp/main.cpp
@@ -0,0 +1,17 @@
+/* 37_03a.c 外部変数の static 指定 */
+#include <stdio.h>
+
+/* 外部変数定義 */
+static int gbl_i = 20;
+
+/* プロトタイプ宣言 */
+void sub(void);
+
+int main(void)
+{
+    printf("main : gbl_i = %d\n", gbl_i++);
+    sub();
+    printf("main : gbl_i = %d\n", gbl_i++);
+
+    return 0;
+}
