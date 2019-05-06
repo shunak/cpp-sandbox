0978717b002e390662e7a27cbb4c306b5a50a326
shunak <shuna966@gmail.com>
2019-05-06 13:45:13 +0900

    190506

---
+++ alg/array_sum.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[] = {4,2,11,4,15,7,9,12,3,6};
+	int i, sum;
+
+	for(i=0;i<sizeof(a)/sizeof(a[0]);i++){
+		printf("a[%d]=%3d\n", i, a[i]);
+	}
+	printf("\n");
+
+	sum=0;
+	for(i=0; i<sizeof(a)/sizeof(a[0]); i++){
+	
+		sum+=a[i];
+	
+	}
+	printf("sum=%3d\n",sum);
+
+
+
+	return 0;
+
+
+
+}
+
